fc5e7f63f77e68565f9ebde78cd2cb789c9a56f3
ASUS <aa>
2020-11-04 06:23:36 +0300

    Initial commit

---
+++ src/ru/mirea/num51/max.java
@@ -0,0 +1,42 @@
+package ru.mirea.num51;
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class max
+{
+    public static int recus(int n)
+    {
+        return n;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        int u=0,sum=0;
+        int i=1;
+        // for (int i =1;i<n+1;i++)
+        while(i<n+1)
+        {
+            u++;
+            for (int j =0;j<u;j++)
+            {
+                sum++;
+                recus(i);
+                System.out.print(recus(i));
+                if (sum==n)
+                {
+                    break;
+                }
+            }
+            if (sum==n)
+            {
+                break;
+            }
+            i++;
+        }
+    }
+
+
+}
